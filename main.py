62b86efd94926470d92a89549b8470e3e775c189
Catcus-p <chhetribarsha537@gmail.com>
2026-05-05 07:56:27 +0545

    ifelse

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def main():
+"""def main():
     age: int = 15
     print(age)
 
@@ -29,4 +29,49 @@ print(f"first 5: {greeting[0:5]}")
 print(f"first 5: {greeting[4:]}")
 if __name__ == "__main__":
     main()
- 
+ """
+
+text = 'hello,world'
+print(text.upper())
+print(text.lower())
+print(text.strip())
+print(text.title())
+print(text.split(','))
+print(text.split('o'))
+var = 'ram'
+greet = "hi"
+print(f"hello{var}{greet}")
+
+is_raining = True
+print(f"Is it raining? {is_raining}")
+
+is_adult = True
+can_travel = False
+go_out = is_adult and can_travel
+
+print(go_out)
+
+var = '1.5'
+
+print(float(var))       
+print(str(var))         
+print(bool(var))         
+
+num = int(float(var))
+print(num)  
+
+if 5==5:
+    print("Equal")
+print("Not equal")
+if True:
+    print("inside")
+print("outside")
+
+weather = "hot"
+temp = 25   
+
+if temp > 20:
+    print(weather)
+else:
+    print("cold")
+
